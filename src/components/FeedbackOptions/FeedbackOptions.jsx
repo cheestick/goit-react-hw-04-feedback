c39f8e13eef6d6265c7e4ff8a477144893996873
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-06-13 10:20:14 +0300

    FeedbackOptions turned to function component

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,26 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import s from './FeedbackOptions.module.css';
 
-class FeedbackOptions extends Component {
-  render() {
-    const { options, onLeaveFeedback } = this.props;
-    return (
-      <div className={s.wrapper}>
-        {options.map(type => (
-          <button
-            key={type}
-            className={s.button}
-            name={type}
-            onClick={() => onLeaveFeedback(type)}
-          >
-            {type}
-          </button>
-        ))}
-      </div>
-    );
-  }
-}
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <div className={s.wrapper}>
+      {options.map(type => (
+        <button
+          key={type}
+          className={s.button}
+          name={type}
+          onClick={() => onLeaveFeedback(type)}
+        >
+          {type}
+        </button>
+      ))}
+    </div>
+  );
+};
 
 FeedbackOptions.propTypes = {
   options: PropTypes.arrayOf(PropTypes.string).isRequired,
